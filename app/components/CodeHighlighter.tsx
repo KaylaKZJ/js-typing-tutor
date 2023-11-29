a69e06d097f6bba37ce58a1d311799d803318369
kayla.joubert <kayla.joubert@fullstack.co.za>
2023-11-29 14:28:44 +0200

    change highlighter

--- app/components/CodeHighlighter.tsx
+++ app/components/CodeHighlighter.tsx
@@ -1,10 +1,10 @@
 'use client';
 import SyntaxHighlighter from 'react-syntax-highlighter';
-import { ocean } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import { nightOwl } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 const CodeHighlighter = ({ children }: { children: string }) => {
   return (
-    <SyntaxHighlighter language='javascript' style={ocean}>
+    <SyntaxHighlighter language='javascript' style={nightOwl}>
       {children}
     </SyntaxHighlighter>
   );

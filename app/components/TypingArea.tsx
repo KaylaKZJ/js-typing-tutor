5147de3d721c2fe96ecdd903edf4710b469fa43b
kayla.joubert <kayla.joubert@fullstack.co.za>
2023-11-29 14:28:58 +0200

    adds autofocus

--- app/components/TypingArea.tsx
+++ app/components/TypingArea.tsx
@@ -1,10 +1,20 @@
 'use client';
 
+import { useEffect, useRef } from 'react';
+
 type TProps = {
   userInput: string;
   setUserInput: React.Dispatch<React.SetStateAction<string>>;
 };
 const TypingArea = ({ setUserInput, userInput }: TProps) => {
+  const inputReference = useRef<any>(null);
+
+  useEffect(() => {
+    if (inputReference.current !== null) {
+      inputReference.current.focus();
+    }
+  }, [inputReference]);
+
   const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
     if (e.code === 'Backspace') {
       setUserInput(userInput.slice(0, -1));
@@ -13,6 +23,7 @@ const TypingArea = ({ setUserInput, userInput }: TProps) => {
 
   return (
     <textarea
+      ref={inputReference}
       value={''}
       className='w-full h-[100vh] p-0 text-2xl bg bg-black'
       onKeyDown={handleKeyDown}
